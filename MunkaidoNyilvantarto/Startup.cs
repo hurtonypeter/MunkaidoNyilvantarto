83cbfaafb4f5bbdafb3adfdb5bcaf53e1a4badbd
Hurtony Péter <hurtonypeter@gmail.com>
2015-11-14 13:19:05 +0100

    init

---
+++ MunkaidoNyilvantarto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MunkaidoNyilvantarto.Startup))]
+namespace MunkaidoNyilvantarto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
